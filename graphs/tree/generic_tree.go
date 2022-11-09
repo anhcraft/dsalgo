7ede021769476f331fea83d88c566243baedac7c
anhcraft <huynhduyanh123123@gmail.com>
2022-11-09 18:11:10 +0700

    Added generic tree

--- graphs/tree/generic_tree.go
+++ graphs/tree/generic_tree.go
@@ -35,17 +35,88 @@ func NewGenericTree[T any](root *GenericTreeNode[T]) *GenericTree[T] {
 	}
 }
 
-// BFS Breadth-first search
 func (t *GenericTree[T]) BFS(f func(val *GenericTreeNode[T])) {
 	queue := sll.NewQueue[*GenericTreeNode[T]](sll.NewQueueNode[*GenericTreeNode[T]](t.Root, nil))
 	for true {
-		ok, head := queue.Poll()
-		if !ok || head == nil {
+		ok, node := queue.Poll()
+		if !ok || node == nil {
 			break
 		}
-		f(head.Value)
-		for _, v := range head.Value.Children {
+		f(node.Value)
+		for _, v := range node.Value.Children {
 			queue.Offer(v)
 		}
+	}
+}
+
+func (t *GenericTree[T]) DFSPreOrder(f func(val *GenericTreeNode[T])) {
+	stack := sll.NewStack[*GenericTreeNode[T]](sll.NewStackNode[*GenericTreeNode[T]](t.Root, nil))
+	for true {
+		ok, node := stack.Poll()
+		if !ok || node == nil {
+			break
+		}
+		f(node.Value)
+		for i := len(node.Value.Children) - 1; i >= 0; i-- {
+			stack.Offer(node.Value.Children[i])
+		}
+	}
+}
+
+func (t *GenericTree[T]) DFSPreOrderReversed(f func(val *GenericTreeNode[T])) {
+	stack := sll.NewStack[*GenericTreeNode[T]](sll.NewStackNode[*GenericTreeNode[T]](t.Root, nil))
+	for true {
+		ok, node := stack.Poll()
+		if !ok || node == nil {
+			break
+		}
+		f(node.Value)
+		for _, v := range node.Value.Children {
+			stack.Offer(v)
+		}
+	}
+}
+
+func (t *GenericTree[T]) DFSPostOrder(f func(val *GenericTreeNode[T])) {
+	ordered := sll.NewStack[*GenericTreeNode[T]](nil)
+	stack := sll.NewStack[*GenericTreeNode[T]](sll.NewStackNode[*GenericTreeNode[T]](t.Root, nil))
+	for true {
+		ok, node := stack.Poll()
+		if !ok || node == nil {
+			break
+		}
+		for _, v := range node.Value.Children {
+			stack.Offer(v)
+		}
+		ordered.Offer(node.Value)
+	}
+	for true {
+		ok, node := ordered.Poll()
+		if !ok || node == nil {
+			break
+		}
+		f(node.Value)
+	}
+}
+
+func (t *GenericTree[T]) DFSPostOrderReversed(f func(val *GenericTreeNode[T])) {
+	ordered := sll.NewStack[*GenericTreeNode[T]](nil)
+	stack := sll.NewStack[*GenericTreeNode[T]](sll.NewStackNode[*GenericTreeNode[T]](t.Root, nil))
+	for true {
+		ok, node := stack.Poll()
+		if !ok || node == nil {
+			break
+		}
+		for i := len(node.Value.Children) - 1; i >= 0; i-- {
+			stack.Offer(node.Value.Children[i])
+		}
+		ordered.Offer(node.Value)
+	}
+	for true {
+		ok, node := ordered.Poll()
+		if !ok || node == nil {
+			break
+		}
+		f(node.Value)
 	}
 }
--- graphs/tree/generic_tree_test.go
+++ graphs/tree/generic_tree_test.go
@@ -20,5 +20,49 @@ func TestGenericTree1(t *testing.T) {
 	tree.BFS(func(e *GenericTreeNode[string]) {
 		list.Append(e.Value)
 	})
-	assert.Equal(t, list.Data, []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"})
+	assert.Equal(t, []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"}, list.Data)
+}
+
+func TestGenericTree2(t *testing.T) {
+	tree := NewGenericTree(NewGenericTreeNode(1))
+	tree.Root.InsertAndGet(NewGenericTreeNode(2)).Insert(NewGenericTreeNode(4), NewGenericTreeNode(5), NewGenericTreeNode(6))
+	tree.Root.InsertAndGet(NewGenericTreeNode(3)).InsertAndGet(NewGenericTreeNode(7)).Insert(NewGenericTreeNode(8), NewGenericTreeNode(9))
+	list := collections.NewArrayList[int]()
+	tree.DFSPreOrder(func(e *GenericTreeNode[int]) {
+		list.Append(e.Value)
+	})
+	assert.Equal(t, []int{1, 2, 4, 5, 6, 3, 7, 8, 9}, list.Data)
+}
+
+func TestGenericTree3(t *testing.T) {
+	tree := NewGenericTree(NewGenericTreeNode(1))
+	tree.Root.InsertAndGet(NewGenericTreeNode(2)).Insert(NewGenericTreeNode(4), NewGenericTreeNode(5), NewGenericTreeNode(6))
+	tree.Root.InsertAndGet(NewGenericTreeNode(3)).InsertAndGet(NewGenericTreeNode(7)).Insert(NewGenericTreeNode(8), NewGenericTreeNode(9))
+	list := collections.NewArrayList[int]()
+	tree.DFSPreOrderReversed(func(e *GenericTreeNode[int]) {
+		list.Append(e.Value)
+	})
+	assert.Equal(t, []int{1, 3, 7, 9, 8, 2, 6, 5, 4}, list.Data)
+}
+
+func TestGenericTree4(t *testing.T) {
+	tree := NewGenericTree(NewGenericTreeNode(1))
+	tree.Root.InsertAndGet(NewGenericTreeNode(2)).Insert(NewGenericTreeNode(4), NewGenericTreeNode(5), NewGenericTreeNode(6))
+	tree.Root.InsertAndGet(NewGenericTreeNode(3)).InsertAndGet(NewGenericTreeNode(7)).Insert(NewGenericTreeNode(8), NewGenericTreeNode(9))
+	list := collections.NewArrayList[int]()
+	tree.DFSPostOrder(func(e *GenericTreeNode[int]) {
+		list.Append(e.Value)
+	})
+	assert.Equal(t, []int{4, 5, 6, 2, 8, 9, 7, 3, 1}, list.Data)
+}
+
+func TestGenericTree5(t *testing.T) {
+	tree := NewGenericTree(NewGenericTreeNode(1))
+	tree.Root.InsertAndGet(NewGenericTreeNode(2)).Insert(NewGenericTreeNode(4), NewGenericTreeNode(5), NewGenericTreeNode(6))
+	tree.Root.InsertAndGet(NewGenericTreeNode(3)).InsertAndGet(NewGenericTreeNode(7)).Insert(NewGenericTreeNode(8), NewGenericTreeNode(9))
+	list := collections.NewArrayList[int]()
+	tree.DFSPostOrderReversed(func(e *GenericTreeNode[int]) {
+		list.Append(e.Value)
+	})
+	assert.Equal(t, []int{9, 8, 7, 3, 6, 5, 4, 2, 1}, list.Data)
 }
